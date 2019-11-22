909162a3dc1fded446754d030ac5a7e88ced9b26
MuJing Tsai <mujingtsai@Mu-Jing.local>
2019-11-22 22:44:30 +0800

    add create course function

--- contracts/DeCourse.sol
+++ contracts/DeCourse.sol
@@ -2,8 +2,10 @@ pragma solidity ^0.5.2;
 
 contract deCourse{
     // mapping(address => uint) public balances;
-    struct member{
+    struct course{
         address teacher;
         mapping (string => address) student;
     }
+    course[]  courses = new course[];
+    // function getCourses public returns () 
 }
