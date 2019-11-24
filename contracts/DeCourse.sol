d046f6497fa97a1797390eeab5baac4231e6deca
Tsai Mujing <moojitsai@gmail.com>
2019-11-25 07:44:35 +0800

    add Enum

--- contracts/DeCourse.sol
+++ contracts/DeCourse.sol
@@ -9,16 +9,26 @@ contract deCourse{
         address teacher;
         address[] students;
     }
+    enum Role { Student, Teacher }
     
     mapping (address => string ) addressToName;
     mapping (address => Course ) addressToCourse;
     
     Course[]  public courses;
     
-    function createCourse  (string memory _role) payable public {   
+    function createCourse  (string memory _title, string memory _description) payable public {   
         address[] memory students;
-        // students.push(msg.sender);
-        // courses.push(new Course(courses.length ,'avn','kjashdkjahskd', students)); 
+        
+        Course memory newCourse = Course({
+            id:courses.length ,
+            title:_title,
+            description:_description,
+            teacher:address(0),
+            students:students
+        });
+        
+        courses.push(newCourse); 
+        courses[courses.length].students.push(msg.sender);
     }
     
     function joinCourse(address _course  , string memory _role) 
