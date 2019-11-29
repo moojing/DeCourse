78d90dc10107aa1fa356619f2076f8b9989ef1bc
Tsai Mujing <moojitsai@gmail.com>
2019-11-29 20:25:25 +0800

    third test

--- test/course.js
+++ test/course.js
@@ -11,22 +11,35 @@ contract("DeCourse", accounts => {
             deployedContractAddress = instance.address;
             DeCourseContract = new web3.eth.Contract(instance.abi, instance.address);
             ownerAddress = await DeCourseContract.methods.owner().call();
-            console.log('ownerAddress: ', ownerAddress);
             assert.equal(accounts[0], ownerAddress, "deployer is not owner");
         })
     );
     
     it('should create a new course', async ()=>{
             
-            web3.eth.getBalance(accounts[0])
-            .then(console.log);
-
+            
             await DeCourseContract.methods.createCourse(
-                'asdasd','asdad',0
+                'The First Course','I am the teahcer in this course',0
             ).send({from:accounts[0],gas:6721975});
+
             let cousese = await   DeCourseContract.methods.courses(0).call() 
+            
             
             assert.equal(0, cousese.id, "deployer is not owner");
        
     })
+
+    it('should join a course as a Student',async function() {
+
+        let courses = await DeCourseContract.methods.getCourses().call();
+        console.log('courses: ', courses);
+        let joinRes = await DeCourseContract.methods.joinCourse(courses[0].id,1).send({from:accounts[1]}) 
+        let newCourses = await DeCourseContract.methods.getCourses().call();
+        console.log('newCourses: ', newCourses);
+        console.log('joinRes: ', joinRes);
+        
+        
+    })
+
+    
 });
